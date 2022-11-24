46e9191e3ec70b6dad87d453672c3bc6ef974c5f
Yuriy Paramonov <yuriy.paramonov@outlook.com>
2022-11-24 07:39:13 +0200

    remove a lot of unused things

--- Assets/AddEdgesOnColider.cs
+++ Assets/AddEdgesOnColider.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Shapes;
 using UnityEngine;
-using UnityEngine.ProBuilder;
 
 [RequireComponent(typeof(Polyline))]
 public class AddEdgesOnColider : MonoBehaviour
