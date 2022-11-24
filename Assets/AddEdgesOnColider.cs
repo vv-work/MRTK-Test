8f0a0b1ebf9f9e28a4e0ac125c64c85c6eaa9c62
Yuriy Paramonov <yuriy.paramonov@outlook.com>
2022-11-24 06:47:25 +0200

    remove all unused references in library

--- Assets/AddEdgesOnColider.cs
+++ Assets/AddEdgesOnColider.cs
@@ -1,33 +1,28 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Plawius.NonConvexCollider;
 using Shapes;
 using UnityEngine;
 using UnityEngine.ProBuilder;
 
-[RequireComponent(typeof(NonConvexColliderComponent))]
 [RequireComponent(typeof(Polyline))]
 public class AddEdgesOnColider : MonoBehaviour
 {
     private  PolylinePoint _polylineReference = new PolylinePoint(Vector2.zero,Color.white,2f);
         
-    private NonConvexColliderComponent _nonConvexColliderComponent;
     private Polyline _polyline;
 
     private void OnDrawGizmosSelected()
     {
+        /*
         Debug.Log($"Hello world");
 
 
-        if (_nonConvexColliderComponent == null)
-            _nonConvexColliderComponent = GetComponent<NonConvexColliderComponent>();
         if (_polyline == null)
             _polyline = GetComponent<Polyline>();
 
         _polyline.points = new List<PolylinePoint>();
 
-        var colliders = _nonConvexColliderComponent.ConvexColliders;
         Dictionary<int, Vector3> lines = new Dictionary<int, Vector3>();
 
         for (int i = 0; i < colliders.Count; i++)
@@ -44,7 +39,9 @@ public class AddEdgesOnColider : MonoBehaviour
                 _polyline.points.Add(clone);
             }
 
+
         }
+        */
     }
 }
 
